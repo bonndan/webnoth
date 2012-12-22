528635a8d9e8dd407cbba8fa21331163bf57bd39
Daniel Pozzi <bonndan76@googlemail.com>
2012-12-22 15:11:22 +0100

    WML map element

---
+++ src/Webnoth/WML/Element/Map.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Webnoth\WML\Element;
+use Webnoth\WML\Element;
+
+/**
+ * Element containing map data
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class Map extends Element
+{
+    /**
+     * width in tiles
+     * @var int
+     */
+    protected $width = null;
+    
+    /**
+     * array of strings referring terrain types
+     * @var array
+     */
+    protected $tiles = array();
+    
+    /**
+     * adds a row of tiles (terrain type strings)
+     * 
+     * @param array $tiles
+     */
+    public function addRawTileRow(array $tiles)
+    {
+        $this->setWidth(count($tiles));
+        foreach ($tiles as $rawTile) {
+            $this->tiles[] = $rawTile;
+        }
+    }
+    
+    /**
+     * Set the width of the map. If the value differs from the current, an exception
+     * is thrown.
+     * 
+     * @param int $width
+     * @throws RuntimeException
+     */
+    protected function setWidth($width)
+    {
+        if ($this->width != $width && $this->width !== null) {
+            throw new \RuntimeException('Width mismatch');
+        }
+        
+        $this->width = (int)$width;
+    }
+    
+    /**
+     * Returns all the tiles.
+     * 
+     * @return array
+     */
+    public function getTiles()
+    {
+        return $this->tiles;
+    }
+}
---
+++ test/src/Webnoth/WML/Element/MapTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Webnoth\WML\Element;
+
+require __DIR__ . '/bootstrap.php';
+
+/**
+ * MapTest
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ * @package Webnoth
+ */
+class MapTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * system under test
+     * @var Map
+     */
+    private $map;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->map = new Map();
+    }
+    
+    public function tearDown()
+    {
+        $this->map = null;
+        parent::tearDown();
+    }
+    
+    /**
+     * 
+     */
+    public function testAddRawTileRow()
+    {
+        $row = array('Gg', 'Gg');
+        $this->map->addRawTileRow($row);
+        $this->assertEquals($row, $this->map->getTiles());
+    }
+    
+    /**
+     * ensures all added tiles are stored
+     */
+    public function testAddRawTileRowTwice()
+    {
+        $this->map->addRawTileRow(array('Gg', 'Gg'));
+        $this->map->addRawTileRow(array('Re', 'Re'));
+        
+        $this->assertEquals(4, count($this->map->getTiles()));
+        $this->assertEquals(array('Gg', 'Gg', 'Re', 'Re'), $this->map->getTiles());
+    }
+    
+    /**
+     * ensures all consecutive calls must have same width
+     */
+    public function testAddRawTileRowException()
+    {
+        $this->map->addRawTileRow(array('Gg', 'Gg'));
+        $this->setExpectedException("\RuntimeException");
+        $this->map->addRawTileRow(array('Gg', 'Gg', 'Gg'));
+    }
+}
