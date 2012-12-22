b997547e6c2caa54dbe4fcd247e136fe5ab4691b
Daniel Pozzi <bonndan76@googlemail.com>
2012-12-22 14:05:52 +0100

    added a console command to parse a terrain WML file

---
+++ consolenoth.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * console app
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+
+use Symfony\Component\Console\Application;
+
+define ('APPLICATION_PATH', __DIR__);
+
+$application = new Application();
+$application->add(new Webnoth\Console\Command\ParseTerrain());
+$application->run();
---
+++ src/Webnoth/Console/Command/ParseTerrain.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * GenerateCommand
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ * @package PPDoc
+ */
+namespace Webnoth\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Usage
+ * =====
+ * "ppdoc generate" takes two arguments:
+ * 
+ * ```
+ * php ppdoc.php generate ./src/ ./ppdoc/
+ * ```
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ * @package PPDoc
+ */
+class ParseTerrain extends Command
+{
+    const FILE = 'file';
+    const DESTINATION_ARG = 'dest';
+    
+    protected function configure()
+    {
+        $this
+            ->setName('parse:terrain')
+            ->setDescription('Parse a terrain.cfg file')
+            ->addArgument(
+                self::FILE,
+                InputArgument::REQUIRED,
+                'Which is the file to parse?'
+            )
+        ;
+    }
+
+    /**
+     * executes the parser
+     * 
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lexer = new \Webnoth\WML\Lexer();
+        
+        $file = $input->getArgument(self::FILE);
+        $output->writeln("Analyzing the contents of $file.");
+        
+        $parser = new \Webnoth\WML\Parser($lexer);
+        $result = $parser->parse(file_get_contents($file));
+        
+        $cache = new \Doctrine\Common\Cache\FilesystemCache(APPLICATION_PATH . '/cache');
+        $cache->save('terrain', $result);
+        
+        //$destination = $input->getArgument(self::DESTINATION_ARG);
+        $output->writeln('Parsed the terrain file successfully.');
+    }
+}
