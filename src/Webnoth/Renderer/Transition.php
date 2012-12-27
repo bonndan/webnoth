93e5740d9aec4ae645586f67d77b1ad0376b23c4
Daniel Pozzi <bonndan76@googlemail.com>
2012-12-27 17:06:05 +0100

    transition class: extracted methods from transitions plugin

--- src/Webnoth/Renderer/Transition.php
+++ src/Webnoth/Renderer/Transition.php
@@ -25,6 +25,12 @@ class Transition
     protected $imageBases = array();
     
     /**
+     * clockwise rotation for traversing surrounding tiles
+     * @var array
+     */
+    protected $rotation = array('n', 'ne', 'se', 's', 'sw', 'nw');
+    
+    /**
      * Factory method, invoked with the image base configuration
      * 
      * @param array $imageBases array(image => maxAdjacentTiles)
@@ -50,4 +56,105 @@ class Transition
         $this->terrainType = $terrainType;
     }
     
+    /**
+     * Pass an array of surrounding terrain differences
+     * 
+     * @param \Webnoth\WML\Collection\TerrainTypes $surrounding, unfiltered
+     * @return array
+     */
+    public function getTransitionImages(\Webnoth\WML\Collection\TerrainTypes $surrounding)
+    {
+        $checkedBase = $this->terrainType->getBaseTerrain();
+        
+        /*
+         * filter, keep only relevant entries
+         */
+        foreach ($surrounding as $direction => $terrain) {
+            /* @var $terrain \Webnoth\WML\Element\TerrainType */
+            $dirBase = $terrain->getBaseTerrain($terrain);
+            
+            if ($checkedBase != $dirBase) {
+                $surrounding[$direction] = null;
+            }
+        };
+        
+        $transitions = array();
+        foreach ($this->imageBases as $image => $maxAdjacent) {
+            $transitions = array_merge($transitions, $this->getTransitionsForImage($surrounding, $image, $maxAdjacent));
+        }
+        
+        return $transitions;
+    }
+    
+    /**
+     * 
+     * @param \Webnoth\WML\Collection\TerrainTypes $surrounding
+     * @param type $image
+     * @param type $maxAdjacent
+     * @return array
+     */
+    protected function getTransitionsForImage(\Webnoth\WML\Collection\TerrainTypes $surrounding, $image, $maxAdjacent) 
+    {
+        if ($maxAdjacent == 1) {
+            return $this->getSeparateTransitionsFor($surrounding->toArray(), $image);
+        } else {
+            return $this->getMergedTransitionsFor($surrounding->toArray(), $image, $maxAdjacent);
+        }
+    }
+    
+    /**
+     * Returns an array of transitions images for each direction.
+     * 
+     * @param array $terrainDirections
+     * @param string $image
+     * @return array
+     */
+    protected function getSeparateTransitionsFor(array $terrainDirections, $image)
+    {
+        $transitions = array();
+        foreach ($this->rotation as $direction) {
+            $terrain = $terrainDirections[$direction];
+            if ($terrain == null) {
+                continue;
+            }
+            $transitions[] = $image . '-' . $direction;
+        }
+        
+        return $transitions;
+    }
+    
+    /**
+     * The merged transitions consist of clusters, i.e. each cluster is made
+     * of continuous transitions without breaks
+     * 
+     * @param array  $directions
+     * @param string $image
+     * @return array
+     */
+    protected function getMergedTransitionsFor(array $directions, $image, $maxAdjacent)
+    {
+        $transitions = array();
+        $tmp         = array();
+        foreach ($this->rotation as $direction) {
+            $terrain = $directions[$direction];
+            if ($terrain != null) {
+                if (count($tmp) == $maxAdjacent) {
+                    $transitions[] = $image . '-' . implode('-', $tmp);
+                    $tmp = array();
+                }
+                $tmp[] = $direction;
+            } else {
+                if (!empty($tmp)) {
+                    $transitions[] = $image . '-' . implode('-', $tmp);
+                }
+                $tmp = array();
+            }
+        }
+        
+        if (!empty($tmp)) {
+            $transitions[] = $image . '-' . implode('-', $tmp);
+        }
+        
+        return $transitions;
+    }
 }
--- test/src/Webnoth/Renderer/TransitionTest.php
+++ test/src/Webnoth/Renderer/TransitionTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Webnoth\Renderer;
 
 require __DIR__ . '/bootstrap.php';
@@ -11,36 +12,110 @@ require __DIR__ . '/bootstrap.php';
  */
 class TransitionTest extends \PHPUnit_Framework_TestCase
 {
+
     /**
      * system  under test
-     * @var ParseTerrain 
+     * @var Transition 
      */
     protected $transition;
-    
+
     /**
      * terrain the transition is responsible for
      * @var ParseTerrain 
      */
     protected $terrainType;
-    
+
     public function setUp()
     {
         $this->terrainType = new \Webnoth\WML\Element\TerrainType();
         $this->terrainType->offsetSet('string', 'Gg');
-        
-        $this->transition = new Transition($this->terrainType);
+
+        $this->transition = Transition::create($this->terrainType, array('some/image' => 3));
     }
-    
+
     public function tearDown()
     {
         $this->transition = null;
         parent::tearDown();
     }
-    
+
     public function testCreate()
     {
-        $res = Transition::create($this->terrainType, array('some/image'));
-        $this->assertInstanceOf("Webnoth\Renderer\Transition", $res);
-        $this->assertAttributeEquals(array('some/image'), 'imageBases', $res);
+        $this->assertInstanceOf("Webnoth\Renderer\Transition", $this->transition);
+        $this->assertAttributeEquals(array('some/image' => 3), 'imageBases', $this->transition);
     }
+
+    /**
+     * 
+     */
+    public function testGetTransitionImagesMerged()
+    {
+        $res = $this->transition->getTransitionImages($this->createSurroundingTerrains());
+        $this->assertInternalType('array', $res);
+        $this->assertContains('some/image-n', $res);
+        $this->assertContains('some/image-sw-nw', $res);
+    }
+    
+    /**
+     * 
+     */
+    public function testGetTransitionImagesMergedWithMaxExceeded()
+    {
+        $this->transition = Transition::create($this->terrainType, array('some/image' => 2));
+        
+        $data = array(
+            'n' => 'Ww',
+            'ne' => 'Ww',
+            'se' => 'Ww',
+            's' => 'Gg',
+            'sw' => 'Gg',
+            'nw' => 'Gg',
+        );
+        
+        
+        $res = $this->transition->getTransitionImages($this->createSurroundingTerrains($data));
+        $this->assertInternalType('array', $res);
+        $this->assertContains('some/image-s-sw', $res);
+        $this->assertContains('some/image-nw', $res);
+    }
+
+    /**
+     * 
+     */
+    public function testGetTransitionImagesSeparated()
+    {
+        $this->transition = Transition::create($this->terrainType, array('some/image' => 1));
+        $res = $this->transition->getTransitionImages($this->createSurroundingTerrains());
+        $this->assertInternalType('array', $res);
+        $this->assertContains('some/image-n', $res);
+        $this->assertContains('some/image-sw', $res);
+        $this->assertContains('some/image-nw', $res);
+    }
+
+    /**
+     * Creates a surrounding terrains collection
+     * 
+     * @param array $data
+     * @return \Webnoth\WML\Collection\TerrainTypes
+     */
+    protected function createSurroundingTerrains(array $data = null)
+    {
+        if ($data === null) {
+            $data = array(
+                'n' => 'Gg',
+                'ne' => 'Ww',
+                'se' => 'Ww',
+                's' => 'Ww',
+                'sw' => 'Gg',
+                'nw' => 'Gg',
+            );
+        }
+
+        $collection = new \Webnoth\WML\Collection\TerrainTypes();
+        foreach ($data as $key => $value) {
+            $collection->set($key, new \Webnoth\WML\Element\TerrainType($value));
+        }
+        return $collection;
+    }
+
 }
