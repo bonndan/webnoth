bf3e5339c76157b20e01524a7836a5b2a05a0aab
Daniel Pozzi <bonndan76@googlemail.com>
2012-12-31 11:01:41 +0100

    introduced a layer class to simplify map layer handling

---
+++ src/Webnoth/WML/Element/Layer.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Webnoth\WML\Element;
+
+use \Webnoth\WML\Collection\TerrainTypes;
+
+/**
+ * A map layer with its own terrain types.
+ * 
+ * @author  Daniel Pozzi <bonndan76@googlemail.com>
+ * @package Webnoth
+ */
+class Layer
+{
+    /**
+     * terrain types for this layer
+     * @var TerrainTypes
+     */
+    protected $terrainTypes = null;
+    
+    /**
+     * array of arrays referring terrain types
+     * @var array y => x
+     */
+    protected $terrains = array();
+
+    /**
+     * Optionally pass the terrain types to use to the constructor.
+     * 
+     * @param \Webnoth\WML\Collection\TerrainTypes $terrainTypes
+     */
+    public function __construct(TerrainTypes $terrainTypes = null)
+    {
+        if ($terrainTypes !== null) {
+            $this->setTerrainTypes($terrainTypes);
+        }
+    }
+    
+    /**
+     * Set the terrain types to use for this layer.
+     * 
+     * @param \Webnoth\WML\Collection\TerrainTypes $terrainTypes
+     */
+    public function setTerrainTypes(TerrainTypes $terrainTypes)
+    {
+        $this->terrainTypes = $terrainTypes;
+    }
+    
+    /**
+     * Returns all the tiles as a stream.
+     * 
+     * @return array
+     */
+    public function getTiles()
+    {
+        $tiles = array();
+        foreach ($this->terrains as $row) {
+            $tiles = array_merge($tiles, $row);
+        }
+        
+        return $tiles;
+    }
+    
+    /**
+	 * return surrounding tiles by direction
+	 * 
+     * @param int $column
+     * @param int $row
+	 * @return \Webnoth\WML\Collection\TerrainTypes
+     * @link http://wiki.wesnoth.org/TerrainGraphicsTutorial#The_hex_coordinate_system
+	 */
+    public function getSurroundingTerrains($column, $row)
+    {
+        if ($column%2) {//odd column
+			$surrounding = array(
+				'ne' => $this->getTerrainAt($column+1, $row),
+				'se' => $this->getTerrainAt($column+1, $row+1),
+				's'  => $this->getTerrainAt($column,   $row+1),
+				'sw' => $this->getTerrainAt($column-1, $row+1),
+				'nw' => $this->getTerrainAt($column-1, $row),
+				'n'  => $this->getTerrainAt($column,   $row-1)
+			);
+        } else {
+			$surrounding = array(
+				'ne' => $this->getTerrainAt($column+1, $row-1),
+				'se' => $this->getTerrainAt($column+1, $row),
+				's'  => $this->getTerrainAt($column,   $row+1),
+				'sw' => $this->getTerrainAt($column-1, $row),
+				'nw' => $this->getTerrainAt($column-1, $row-1),
+				'n'  => $this->getTerrainAt($column,   $row-1)
+			);
+        }
+        
+        return $this->toTerrainTypeCollection($surrounding);
+    }
+    
+    /**
+     * Converts an terrain string array into a terraintype collection.
+     * 
+     * @param array $collection
+     * @return \Webnoth\WML\Collection\TerrainTypes
+     */
+    protected function toTerrainTypeCollection(array $collection)
+    {
+        foreach ($collection as $key => $terrain) {
+            $collection[$key] = $this->terrainTypes->get($terrain);
+        }
+            
+        return new \Webnoth\WML\Collection\TerrainTypes($collection);
+    }
+    
+    /**
+     * Returns the terrain at a given offset
+     * 
+     * @param int    $column
+     * @param int    $row
+     * @return string|null
+     */
+    public function getTerrainAt($column, $row)
+    {
+        if (!isset($this->terrains[$row][$column])) {
+            return null;
+        }
+        return $this->terrains[$row][$column];
+    }
+    
+    /**
+     * Set a specific terrain at a coordinate.
+     * 
+     * @param int    $column
+     * @param int    $row
+     * @param string $terrain
+     */
+    public function setTerrainAt($column, $row, $terrain)
+    {
+        $this->terrains[$row][$column] = $terrain;
+    }
+}
---
+++ test/src/Webnoth/WML/Element/LayerTest.php
@@ -0,0 +1,177 @@
+<?php
+namespace Webnoth\WML\Element;
+
+require __DIR__ . '/bootstrap.php';
+
+/**
+ * LayerTest
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ * @package Webnoth
+ */
+class LayerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * system under test
+     * @var Layer
+     */
+    private $layer;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->layer = new Layer($this->getCache()->fetch('terrain'));
+    }
+    
+    public function tearDown()
+    {
+        $this->layer = null;
+        parent::tearDown();
+    }
+    
+    /**
+     * Ensures the terrain type can be grabbed by coordinates
+     */
+    public function testGetTerrainAt()
+    {
+        $this->layer->setTerrainAt(1, 1, '11');
+        $this->assertEquals('11', $this->layer->getTerrainAt(1, 1));
+        $this->layer->setTerrainAt(1, 2, '12');
+        $this->assertEquals('12', $this->layer->getTerrainAt(1, 2));
+    }
+    
+    /**
+     * Ensures off-map coords return null
+     */
+    public function testGetTerrainAtReturnsVoid()
+    {
+        $this->assertNull($this->layer->getTerrainAt(1, 2));
+    }
+    
+    /**
+     * Ensures the surrounding terrains are returned properly
+     */
+    public function testGetSurroundingTerrainsForEvenCol()
+    {
+        $this->prepareLayerForSurroundingTilesTest();
+        $surrounding = $this->layer->getSurroundingTerrains(2, 2);
+        $this->assertInstanceOf("\Webnoth\WML\Collection\TerrainTypes", $surrounding);
+        $expected = array(
+            'ne' => '31',
+            'se' => '32',
+            's'  => '23',
+            'sw' => '12',
+            'nw' => '11',
+            'n'  => '21'
+        );
+        $this->assertCollectionEquals($expected, $surrounding);
+    }
+    
+    /**
+     * Ensures the surrounding terrains are returned properly
+     */
+    public function testGetSurroundingTerrainsForOddCol()
+    {
+        $this->prepareLayerForSurroundingTilesTest();
+        $surrounding = $this->layer->getSurroundingTerrains(1, 1);
+        
+        $this->assertInstanceOf("\Webnoth\WML\Collection\TerrainTypes", $surrounding);
+        $expected = array(
+            'ne' => '21',
+            'se' => '22',
+            's'  => '12',
+            'sw' => '02',
+            'nw' => '01',
+            'n'  => '10'
+        );
+        $this->assertCollectionEquals($expected, $surrounding);
+    }
+    
+    /**
+     * Sets required tiles.
+     */
+    protected function prepareLayerForSurroundingTilesTest()
+    {
+        $this->layer->setTerrainAt(0, 0, '00');
+        $this->layer->setTerrainAt(1, 0, '10');
+        $this->layer->setTerrainAt(2, 0, '20');
+        $this->layer->setTerrainAt(3, 0, '30');
+        
+        $this->layer->setTerrainAt(0, 1, '01');
+        $this->layer->setTerrainAt(1, 1, '11');
+        $this->layer->setTerrainAt(2, 1, '21');
+        $this->layer->setTerrainAt(3, 1, '31');
+        
+        $this->layer->setTerrainAt(0, 2, '02');
+        $this->layer->setTerrainAt(1, 2, '12');
+        $this->layer->setTerrainAt(2, 2, '22');
+        $this->layer->setTerrainAt(3, 2, '32');
+        
+        $this->layer->setTerrainAt(0, 3, '03');
+        $this->layer->setTerrainAt(1, 3, '13');
+        $this->layer->setTerrainAt(2, 3, '23');
+        $this->layer->setTerrainAt(3, 3, '33');
+        
+        $terrainTypes = $this->createFakeTerrainLookup($this->layer);
+        $this->layer->setTerrainTypes($terrainTypes);
+    }
+    
+    /**
+     * assert a terraintype collection matches the given strings
+     * @param type $expected
+     * @param \Webnoth\WML\Collection\TerrainTypes $collection
+     */
+    protected function assertCollectionEquals($expected, \Webnoth\WML\Collection\TerrainTypes $collection)
+    {
+        $collection = $collection->toArray();
+        foreach ($expected as $key => $value) {
+            $this->assertArrayHasKey($key, $collection);
+            $this->assertInstanceOf("\Webnoth\WML\Element\TerrainType", $collection[$key]);
+            $this->assertEquals($value, $collection[$key]->getString());
+        }
+    }
+    
+    /**
+     * creates a fake terrain lookup collection
+     * 
+     * @param Layer $map
+     * @return \Webnoth\WML\Collection\TerrainTypes
+     */
+    protected function createFakeTerrainLookup(Layer $map)
+    {
+        $collection = new \Webnoth\WML\Collection\TerrainTypes();
+        foreach ($map->getTiles() as $terrain) {
+            $collection->set($terrain, new \Webnoth\WML\Element\TerrainType($terrain));
+        }
+        
+        return $collection;
+    }
+    
+    public function testGetTiles()
+    {
+        $this->prepareLayerForSurroundingTilesTest();
+        
+        $tiles = $this->layer->getTiles();
+        $this->assertEquals(16, count($tiles));
+    }
+    
+    /**
+     * Ensures terrains can be set to the heightmap.
+     */
+    public function testSetTerrain()
+    {
+        $this->layer->setTerrainAt(1, 1, 'Gg');
+        $this->assertAttributeEquals(array(1 => array(1 => 'Gg')), 'terrains', $this->layer);
+    }
+    
+    /**
+     * creates a cache instance
+     * 
+     * @return \Doctrine\Common\Cache\FilesystemCache
+     */
+    protected function getCache()
+    {
+        $cache = new \Doctrine\Common\Cache\FilesystemCache(APPLICATION_PATH . '/cache');
+        return $cache;
+    }
+}
